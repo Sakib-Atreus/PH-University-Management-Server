a55288b309e3785edddc3ad88ca08065b7b1b69e
Sakib-Atreus <sakibatreus@gmail.com>
2024-06-10 16:20:27 +0600

    offered course validation added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { UserControllers } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { createStudentValidationSchema } from '../student/student.validation';
+import { createFacultyValidationSchema } from '../Faculty/faculty.validation';
+import { createAdminValidationSchema } from '../Admin/admin.validation';
 
 const router = express.Router();
 
@@ -9,6 +11,18 @@ router.post(
   '/create-student',
   validateRequest(createStudentValidationSchema),
   UserControllers.createStudent,
+);
+
+router.post(
+  '/create-faculty',
+  validateRequest(createFacultyValidationSchema),
+  UserControllers.createFaculty,
+);
+
+router.post(
+  '/create-admin',
+  validateRequest(createAdminValidationSchema),
+  UserControllers.createAdmin,
 );
 
 export const UserRoutes = router;
